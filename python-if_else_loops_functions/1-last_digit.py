9b0ed25c6ebddc343d08ad648f617b00621b85bb
LucasN <lucas.n230896@gmail.com>
2026-01-13 15:00:25 +0000

    fix: pycodestyle validation, line too long

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
+last_dgt = number % 10
 
 if number < 0:
-    last_digit = number % -10
+    last_dgt = number % -10
 
-if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+if last_dgt > 5:
+    print(f"Last digit of {number} is {last_dgt} and is greater than 5")
 
-if last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
+if last_dgt == 0:
+    print(f"Last digit of {number} is {last_dgt} and is 0")
 
-if last_digit < 6 and last_digit != 0:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+if last_dgt < 6 and last_dgt != 0:
+    print(f"Last digit of {number} is {last_dgt} and is less than 6 and not 0")
