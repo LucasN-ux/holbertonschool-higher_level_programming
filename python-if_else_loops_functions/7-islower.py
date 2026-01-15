50db1f75ae4abe78a401dbeb4ab03f9213069610
LucasN <lucas.n230896@gmail.com>
2026-01-15 08:18:19 +0000

    fix: ord(c) condition

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 def islower(c):
-    for i in range(ord('a'), ord('z') + 1):
-        if c == i:
-            return True
-        else:
-            return False
+    if ord(c) in range(ord('a'), ord('z') + 1):
+        return True
+    else:
+        return False
