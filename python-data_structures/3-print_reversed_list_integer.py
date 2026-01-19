f6a79b38441e20025ce3658c9cc6346f91f875b3
LucasN <lucas.n230896@gmail.com>
2026-01-19 10:06:46 +0000

    fix: use my_list 0 to descreament i

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     i = len(my_list)
-    while i > 0:
+    while i >= my_list[0]:
         print("{:d}".format(i))
         i = i - 1
