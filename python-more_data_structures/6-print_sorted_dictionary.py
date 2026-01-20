8251959b7c7f2b13c5979eb10ef762c675241373
LucasN <lucas.n230896@gmail.com>
2026-01-20 14:52:52 +0000

    feat: prints a dictionary by ordered keys.

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    print("Number: {}".format(a_dictionary.get('Number')))
-    print("ids: {}".format(a_dictionary.get('ids')))
-    print("language: {}".format(a_dictionary.get('language')))
-    print("track: {}".format(a_dictionary.get('track')))
+    out = sorted(a_dictionary)
+    for key in out:
+        print("{}: {}".format(key, a_dictionary.get(key)))
