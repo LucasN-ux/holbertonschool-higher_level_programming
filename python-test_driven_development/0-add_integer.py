ef99b2ab0c2be3e33af5d57cb541d3e66b05b6fa
LucasN <lucas.n230896@gmail.com>
2026-01-23 09:43:43 +0000

    fix: pycode style

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -12,7 +12,7 @@ def add_integer(a, b=98):
     for name, value in (("a", a), ("b", b)):
         if type(value) not in [int, float]:
             raise TypeError(f"{name} must be an integer")
-        if isinstance(value, float) and (value != value or \
+        if isinstance(value, float) and (value != value or
            value in (float("inf"), float("-inf"))):
             raise TypeError(f"{name} must be an integer")
 
