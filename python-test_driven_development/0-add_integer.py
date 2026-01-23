b89ed910e52691bc543d4f9a735e9d2b8aa46391
LucasN <lucas.n230896@gmail.com>
2026-01-23 09:42:53 +0000

    fix: change function to passed TDD

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -9,10 +9,11 @@ def add_integer(a, b=98):
     Adds two integers
     """
 
-    if type(a) not in [int, float]:
-        raise TypeError("a must be an integer")
-
-    if type(b) not in [int, float]:
-        raise TypeError("b must be an integer")
+    for name, value in (("a", a), ("b", b)):
+        if type(value) not in [int, float]:
+            raise TypeError(f"{name} must be an integer")
+        if isinstance(value, float) and (value != value or \
+           value in (float("inf"), float("-inf"))):
+            raise TypeError(f"{name} must be an integer")
 
     return int(a) + int(b)
