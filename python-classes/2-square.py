279becf12e89535c38bcadcea27972fb589a3571
LucasN <lucas.n230896@gmail.com>
2026-01-27 12:03:46 +0000

    feat: size must be an integer and >= 0

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    Class that defines a square
+    """
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("syze must be an integer")
+        if int(size) < 0:
+            raise ValueError("size must be >= 0")
