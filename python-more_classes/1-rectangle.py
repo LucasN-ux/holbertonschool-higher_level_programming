2e2d7a9f51e57e67a8f1c00f6fa66057cf49eca3
LucasN <lucas.n230896@gmail.com>
2026-01-29 12:14:26 +0000

    feat: getter and setter width and height

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+Module thaan define a rectangle
+"""
+
+
+class Rectangle:
+    """
+    Class than define a rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """
+        Init method
+
+        :param width: Width of rectangle
+        :param height: Height of rectangle
+        """
+        if not isinstance(width, int) or not isinstance(height, int):
+            raise TypeError("width must be an integer")
+        if width < 0 or height < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+        Method who return the private width
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """
+        Method who return the private height
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter to change value of width
+
+        :param value: width value
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """
+        Setter to change value of height
+
+        :param value: height value
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+            self.__height = value
