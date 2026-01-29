ed647afe4fd3a049a9d5ef0c319ccb61a761d463
LucasN <lucas.n230896@gmail.com>
2026-01-29 12:19:10 +0000

    fix: change init raise Error

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -15,9 +15,11 @@ class Rectangle:
         :param width: Width of rectangle
         :param height: Height of rectangle
         """
-        if not isinstance(width, int) or not isinstance(height, int):
+        if (not isinstance(width, int) or
+                not isinstance(height, int)):
             raise TypeError("width must be an integer")
-        if width < 0 or height < 0:
+        if (width < 0 or
+                height < 0):
             raise ValueError("width must be >= 0")
         self.__width = width
         self.__height = height
