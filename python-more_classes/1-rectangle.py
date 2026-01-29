9f4b5b8efcc5dd154c92dbbecc70372f4a2a8f48
LucasN <lucas.n230896@gmail.com>
2026-01-29 12:27:17 +0000

    fix: self are not in if condition

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -51,7 +51,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-            self.__width = value
+        self.__width = value
 
     @height.setter
     def height(self, value):
@@ -64,4 +64,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-            self.__height = value
+        self.__height = value
