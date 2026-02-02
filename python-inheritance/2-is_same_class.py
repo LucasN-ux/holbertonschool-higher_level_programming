35c80ba568ec970651869a18831e9287e4624998
LucasN <lucas.n230896@gmail.com>
2026-02-02 15:21:43 +0000

    fix: add description function

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def is_same_class(obj, a_class):
+    """ object is exactly an instance of the specified class"""
     if type(obj) == a_class:
         return True
     return False
