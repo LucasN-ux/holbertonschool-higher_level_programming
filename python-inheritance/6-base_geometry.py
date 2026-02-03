612abc0cc1d21128cd19da792331946d6430236a
LucasN <lucas.n230896@gmail.com>
2026-02-03 09:47:09 +0000

    feat: methode than Raises en exception because area is not implemented

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""ModuleDefines a BaseGeometry class """
+
+
+class BaseGeometry():
+    """Represents a base geometry class"""
+    def area(self):
+        """Raises en exception because area is not implemented"""
+        raise Exception("area() is not implemented")
