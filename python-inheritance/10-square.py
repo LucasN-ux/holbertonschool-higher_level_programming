40a900d06b03ec5107b4c8eba51dbfff01162e1e
LucasN <lucas.n230896@gmail.com>
2026-02-04 09:12:46 +0000

    fix: import 9-rectangle

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -2,36 +2,7 @@
 """ModuleDefines a BaseGeometry class """
 
 
-class BaseGeometry():
-    """Represents a base geometry class"""
-    def area(self):
-        """Raises en exception because area is not implemented"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates the value"""
-        if not isinstance(value, int) or isinstance(value, bool):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """class inherits who define a rectangle"""
-    def __init__(self, width, height):
-        """init width and height of the rectangle"""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """returns the area of the rectangle"""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """returns string"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
