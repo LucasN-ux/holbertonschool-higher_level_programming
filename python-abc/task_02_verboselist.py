ee2f013de24bad90f378ec31d5af16b65c7cfc83
LucasN <lucas.n230896@gmail.com>
2026-02-04 12:58:57 +0000

    feat: add class contain different method on a class list

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""setting an Abstract class"""
+
+
+class VerboseList(list):
+    """class named VerboseList that extends the Python list class"""
+    def append(self, item):
+        """append method"""
+        super().append(item)
+        print("Added [{}] to the list.".format(item))
+
+    def extend(self, item):
+        """extend method"""
+        super().extend(item)
+        print("Extended the list with [{}] items.".format(len(item)))
+
+    def remove(self, item):
+        """remove method"""
+        super().remove(item)
+        print("Removed [{}] from the list.".format(item))
+
+    def pop(self, item=-1):
+        """pop method"""
+        item = super().pop(item)
+        print("Popped [{}] from the list.".format(item))
