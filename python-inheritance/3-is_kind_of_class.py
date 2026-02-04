09194788d55545ad8d9a0cefc52cfce94f3b95c9
LucasN <lucas.n230896@gmail.com>
2026-02-04 08:52:27 +0000

    fix: pycode style

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""script than contain function"""
+
+
 def is_kind_of_class(obj, a_class):
     """ object is an instance of an instance of the specified class"""
     if isinstance(obj, a_class):
