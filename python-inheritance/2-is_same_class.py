eb45cb5981a91fd95894fa7b92b63298e90c14a9
LucasN <lucas.n230896@gmail.com>
2026-02-04 08:50:02 +0000

    fix: add 'is' indentation

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""script than contain function"""
+
+
 def is_same_class(obj, a_class):
     """ object is exactly an instance of the specified class"""
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     return False
