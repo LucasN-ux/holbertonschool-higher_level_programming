79077a2aba069998625c82e744dc9125ffb489a1
LucasN <lucas.n230896@gmail.com>
2026-02-04 08:40:28 +0000

    fix: add description scipt and Spaces size

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""script contain function"""
+
 
 def lookup(obj):
-    """  returns the list of available attributes and methods of an object """
+    """returns the list of available attributes and methods of an object """
     return dir(obj)
