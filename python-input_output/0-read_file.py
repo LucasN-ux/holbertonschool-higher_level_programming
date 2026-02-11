1db2b08acaace46323b5ab2f55beb8cfaffa9926
LucasN <lucas.n230896@gmail.com>
2026-02-11 08:28:40 +0000

    fix: change to UTF-8 uppercase

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,5 +2,5 @@
 
 def read_file(filename=""):
     """Reads a text file and prints it to stout"""
-    with open(filename, "r", encoding="utf-8") as f:
-        print(f.read(), end="")
+    with open(filename, "r", encoding="UTF-8") as f:
+        print(f.read())
--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -4,5 +4,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """writes an Object to a text file"""
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename, "w", encoding="UTF-8") as f:
         return json.dump(my_obj, f)
