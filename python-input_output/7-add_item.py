2edb51dd5e2c083e615125a38cf02e265f7bbbb1
LucasN <lucas.n230896@gmail.com>
2026-02-11 09:02:57 +0000

    fix: If the file doesn't exist, it should be created

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,13 +3,13 @@
 and then save them to a file"""
 import json
 import sys
-
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
 file = "add_item.json"
-my_obj = load_from_json_file(file)
+try:
+    my_obj = load_from_json_file(file)
+except Exception:
+    my_obj = []
 my_obj.extend(sys.argv[1:])
 save_to_json_file(my_obj, file)
