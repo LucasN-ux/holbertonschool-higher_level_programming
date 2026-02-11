ac535857e7a52ed15b112f284b5fbb61afd0f80a
LucasN <lucas.n230896@gmail.com>
2026-02-11 08:42:43 +0000

    fix: adding end to read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """Reads a text file and prints it to stout"""
     with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read())
+        print(f.read(), end="")
