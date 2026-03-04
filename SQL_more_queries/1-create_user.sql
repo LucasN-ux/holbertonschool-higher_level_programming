aac11afed17d354dfbb5f0a1942925f5c7dd3ae8
LucasN-ux <lucas.n230896@gmail.com>
2026-03-04 15:37:10 +0100

    feat: user with all privileges

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- To create a MySQL user with all privileges, you can use the following commands:
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
