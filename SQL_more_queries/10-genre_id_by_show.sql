03999dc83c30392ac3fb833a29933d9ad71cea21
LucasN-ux <lucas.n230896@gmail.com>
2026-03-05 15:04:38 +0100

    feat: script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+--  Write a SQL query to find the title and genre_id of all TV shows. The tv_shows table has the following columns: id, title. The tv_show_genres table has the following columns: show_id, genre_id. The show_id column in the tv_show_genres table is a foreign key that references the id column in the tv_shows table.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
