46f086a9b15469b194459f85dfc35952f0fea932
LucasN-ux <lucas.n230896@gmail.com>
2026-03-05 16:27:54 +0100

    fix: Count shows by genre

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Write a SQL query to find the name of each genre and the number of TV shows that belong to that genre.
-SELECT g.name AS genre, COUNT(sg.show_id) AS number_of_shows
-FROM genres g
-JOIN tv_show_genres sg ON g.id = sg.genre_id
-GROUP BY g.id
-HAVING COUNT(sg.show_id) > 0
+SELECT tv_genres.name AS genre,
+COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
