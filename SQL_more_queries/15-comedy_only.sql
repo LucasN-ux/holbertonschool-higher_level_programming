84d41db1fd4fdef2d539b871a1f8b5de3f739ff9
LucasN-ux <lucas.n230896@gmail.com>
2026-03-05 16:29:30 +0100

    fix: Write a SQL query to find the title of all TV shows that belong to the "Comedy" genre

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- Write a SQL query to find the title of all TV shows that belong to the "Comedy" genre
-SELECT s.title
-FROM tv_shows s
-JOIN tv_show_genres sg ON s.id = sg.show_id
-JOIN genres g ON sg.genre_id = g.id
-WHERE g.name = 'Comedy'
-ORDER BY s.title ASC;
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = "Comedy" AND tv_show_genres.genre_id IS NOT NULL
+ORDER BY tv_shows.title ASC;
