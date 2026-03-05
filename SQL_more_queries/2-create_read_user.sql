547089f1e5c5f74b244c51db00f3b706546b783a
LucasN-ux <lucas.n230896@gmail.com>
2026-03-05 09:30:51 +0100

    feat: creates the database hbtn_0d_2 and the user user_0d_2

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- To create a MySQL user with only SELECT privileges on a specific database, you can use the following commands:
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
