93714c50e2c21d4438ced493c1e12aa5878f27d5
LucasN-ux <lucas.n230896@gmail.com>
2026-03-06 14:51:03 +0100

    fix: order by states.id

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
+"""
+Script that lists all states with a name starting
+with N (upper N) from the database hbtn_0e_0_usa
+"""
 import MySQLdb
 import sys
 
@@ -17,7 +20,8 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' \
+                ORDER BY states.id ASC")
 
     rows = cur.fetchall()
 
