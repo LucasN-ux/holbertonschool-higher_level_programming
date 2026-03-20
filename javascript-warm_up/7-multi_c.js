5ddb49a97212d1fe70015a0b2b927ba805e95a75
LucasN-ux <lucas.n230896@gmail.com>
2026-03-20 10:48:31 +0100

    fix: missing if no arg

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-const x = process.argv.slice(2);
-if (x.length === 0 ) {
+const x = process.argv.slice[2];
+if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  const n = parseInt(x[0]);
-  if (isNaN(n)) {
-    console.log('Missing number of occurrences');
-  } else {
-    for (let i = 0; i < n; i++) {
-      console.log('C is fun');
-    }
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
   }
 }
