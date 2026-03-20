ea97b02bd943a4173e1360bdd8d04dc2a5c81568
LucasN-ux <lucas.n230896@gmail.com>
2026-03-20 10:51:37 +0100

    fix: add two const variable

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const x = process.argv.slice[2];
-if (isNaN(x)) {
+const args = process.argv.slice(2);
+const data = args[0];
+if (isNaN(Number(data))) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < x; i++) {
+  for (let i = 0; i < data; i++) {
     console.log('C is fun');
   }
 }
